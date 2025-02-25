a0fa8299a2e9553c1a1bdfc991f252b033a6072a
Mihail9595 <mihailkokskokin@gmail.com>
2025-02-25 18:37:02 +0300

    first commit

---
+++ src/components/Categories.jsx
@@ -0,0 +1,30 @@
+const Categories = ({ value, onChangeCategory }) => {
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((item, index) => {
+          return (
+            <li
+              key={index}
+              onClick={() => onChangeCategory(index)}
+              className={value === index ? "active" : ""}
+            >
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
---
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,24 @@
+import ContentLoader from "react-content-loader"
+
+const Skeleton = (props) => (
+  <div className="pizza-block-wrapper">    
+    <ContentLoader className="pizza-block"
+      speed={2}
+      width={280}
+      height={465}
+      viewBox="0 0 280 465"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+      {...props}
+    >
+    <circle cx="135" cy="125" r="109" /> 
+    <rect x="0" y="251" rx="10" ry="10" width="280" height="22" /> 
+    <rect x="0" y="289" rx="10" ry="10" width="280" height="88" /> 
+    <rect x="0" y="401" rx="10" ry="10" width="84" height="37" /> 
+    <rect x="36" y="428" rx="0" ry="0" width="9" height="4" /> 
+    <rect x="135" y="400" rx="10" ry="10" width="142" height="37" />
+  </ContentLoader>
+  </div>
+  )
+  
+  export default Skeleton
